1c9505309717f37ce0fcd7bff9bab03be734fb8a
Kim Ravn Hansen <krh@krh-workstation.internal>
2016-07-27 08:31:55 +0200

    Better testing

--- spec/Moccalotto/Hayttp/RequestSpec.php
+++ spec/Moccalotto/Hayttp/RequestSpec.php
@@ -2,8 +2,10 @@
 
 namespace spec\Moccalotto\Hayttp;
 
-use Moccalotto\Hayttp\Request;
+use SimpleXmlElement;
 use PhpSpec\ObjectBehavior;
+use Moccalotto\Hayttp\Request;
+use Moccalotto\Hayttp\Contracts\Request as RequestContract;
 
 class RequestSpec extends ObjectBehavior
 {
@@ -11,13 +13,38 @@ class RequestSpec extends ObjectBehavior
     {
         $this->beConstructedWith('post', 'https://example.org');
         $this->shouldHaveType(Request::class);
+        $this->shouldHaveType(RequestContract::class);
     }
 
     public function it_posts_json()
     {
         $this->beConstructedThrough('post', ['https://example.org']);
-        $this->shouldHaveType(Request::class);
 
-        $this->sendsJson(['this' => 'object', 'will' => 'be', 'converted' => 'to json'])->shouldHaveType(Request::class);
+        $data = ['this' => 'array', 'will' => 'be', 'conterted' => 'to', 'json' => 'object'];
+
+        $req = $this->sendsJson($data);
+
+        $req->shouldHaveType(RequestContract::class);
+
+        $req->render()->shouldContain(
+            json_encode($data, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES)
+        );
+
+        $req->render()->shouldContain('Content-Type: application/json');
+    }
+
+    public function it_posts_xml()
+    {
+        $this->beConstructedThrough('post', ['https://example.org']);
+
+        $data = new SimpleXmlElement('<root></root>');
+
+        $req = $this->sendsXml($data);
+
+        $req->shouldHaveType(Request::class);
+
+        $req->render()->shouldContain($data->asXml());
+
+        $req->render()->shouldContain('Content-Type: application/xml');
     }
 }
