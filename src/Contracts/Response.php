1372a47f974e1c550cdd270d080e49b2bedf3ca2
Kim Ravn Hansen <moccalotto@gmail.com>
2016-08-16 19:34:16 +0200

    Flesh out the response contract.

--- src/Contracts/Response.php
+++ src/Contracts/Response.php
@@ -11,12 +11,70 @@
 
 namespace Moccalotto\Hayttp\Contracts;
 
+use SimpleXmlElement;
+
 interface Response
 {
+    /**
+     * Get the request that produced this response.
+     *
+     * @return RequestContract
+     */
+    public function request() : Request;
+
+    /**
+     * Get the HTTP Response Code.
+     *
+     * @return string
+     */
+    public function statusCode() : string;
+
+    /**
+     * Get the http reason phrase.
+     *
+     * @return string
+     */
+    public function reasonPhrase() : string;
+
+    /**
+     * Get the headers.
+     *
+     * @return string[]
+     */
+    public function headers() : array;
+
+    /**
+     * Get the response body.
+     *
+     * @return string
+     */
+    public function body() : string;
+
+    /**
+     * Parse the body as json and return it as a PHP value.
+     *
+     * @return mixed - array or StdClass
+     */
+    public function decodedJson();
+
+    /**
+     * Parse the body as xml and return it as a SimpleXmlElement.
+     *
+     * @return SimpleXmlElement
+     */
+    public function decodedXml() : SimpleXmlElement;
+
     /**
      * Get the entire response, including headers, as a string.
      *
      * @return string
      */
     public function render() : string;
+
+    /**
+     * Cast to string.
+     *
+     * @return string
+     */
+    public function __toString();
 }
